0320579f24bda90e30a988dec6245acd65f63b82
Gloryarty <Gloryartydev@gmail.com>
2026-03-20 13:46:06 +0600

    upd

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const repoName = "NEX-Platforms";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
     unoptimized: true,
   },
